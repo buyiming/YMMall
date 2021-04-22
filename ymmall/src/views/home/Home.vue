80e83c85bc167dd347930cdec68dc66ab94d6e59
BYM <1532220520@qq.com>
2021-04-22 22:04:04 +0800

    添加轮播图

--- ymmall/src/views/home/Home.vue
+++ ymmall/src/views/home/Home.vue
@@ -1,13 +1,46 @@
 <template>
-  <h1>首页</h1>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banner"></home-swiper>
+  </div>
 </template>
 
 <script>
+
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./homeComps/HomeSwiper";
+import {getHomeMultidata} from "network/home";
+
+
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    HomeSwiper,
+    NavBar
+  },
+  data(){
+    return{
+      banner:[],
+      recommend:[],
+    }
+  },
+  created() {
+    //请求主页数据
+    getHomeMultidata().then(res => {
+      console.log(res);
+      this.banner = res.data.banner.list;
+      this.recommend = res.data.recommend.list;
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
---
+++ ymmall/src/views/home/homeComps/HomeSwiper.vue
@@ -0,0 +1,32 @@
+<template>
+  <swiper>
+    <swiper-item v-for="(item,id) in banners" :key="id">
+      <a :href="item.link">
+        <img :src="item.image">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+
+import Swiper from "components/common/swiper/Swiper";
+import SwiperItem from "components/common/swiper/SwiperItem";
+
+export default {
+  name: "HomeSwiper",
+  components: {SwiperItem, Swiper},
+  props:{
+    banners:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
